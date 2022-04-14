a81b58b5554746595aa9af1376afaf55c4b96042
Mateusz Kotwiński <mkotwinski98@gmail.com>
2022-04-14 06:44:09 +0200

    Add error handling

--- app.js
+++ app.js
@@ -14,5 +14,11 @@ app.use(express.static('public'));
 
 app.use(authRoutes);
 
-db.connectToDatabase();
-app.listen(3000);
+db.connectToDatabase()
+  .then(function () {
+    app.listen(3000);
+  })
+  .catch(function (error) {
+  console.log('Failed to connect to the database');
+  console.log(error);
+});
