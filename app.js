5b66e0022f77346a53b3c7b88666a72e6e503674
Mateusz Kotwiński <mkotwinski98@gmail.com>
2022-04-16 09:00:49 +0200

    Add session and fix syntax error

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const path = require('path');
 
 const express = require('express');
-const csurf = require('csurf');
+const csrf = require('csurf');
+const expressSession = require('express-session');
 
+const createSessionConfig = require('./config/session');
 const db = require('./data/database');
 const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
@@ -16,7 +18,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
-app.use(csurf());
+const sessionConfig = createSessionConfig();
+
+app.use(expressSession(sessionConfig));
+app.use(csrf());
 
 app.use(addCsrfTokenMiddleware);
 
